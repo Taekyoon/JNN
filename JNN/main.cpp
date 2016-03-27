024ec8326e83378928f97cbaff4cf249f9dfe670
junodeveloper <wnsgh224@naver.com>
2016-03-28 02:10:46 +0900

    First Commit

--- JNN/main.cpp
+++ JNN/main.cpp
@@ -1,15 +1,6 @@
-//
-//  main.cpp
-//  JNN
-//
-//  Created by 황준호 on 2016. 3. 28..
-//  Copyright © 2016년 황준호. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/* 2016.03.28 started by junodeveloper
+ */
+int main() {
+    
     return 0;
 }
